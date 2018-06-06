395378210cf9e59d3c25fa0d74f28843f0c5d244
Rushdi97 <34390284+Rushdi97@users.noreply.github.com>
2018-06-06 01:21:02 -0500

    Update DartThrowingAlgorithm.py

--- DartThrowingAlgorithm.py
+++ DartThrowingAlgorithm.py
@@ -1,18 +1,23 @@
+#calculate pi via dart throwing algorithm
+
 import random 
 import math
 import sys
 
-numDarts = int(sys.argv[1])#obtain how many darts to be thrown(cmd line arg)
+#obtain how many darts to be thrown(cmd line arg)
+numDarts = int(sys.argv[1])
 
+#keeps track of what hits the dart board
 hits = 0
 
-for x in range(numDarts):
+for x in range(numDarts + 1):
 	#obtain x and y coords 
 	x=random.random();
 	y=random.random();
 	#find distance
 	dist=math.sqrt(x*x + y*y)
 	
+	#determine if it hit the board and if so increment hits by 1
 	if dist < 1 or dist ==1:
 		hits = hits + 1
 		
